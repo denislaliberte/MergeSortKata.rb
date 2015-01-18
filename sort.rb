7ba65b7845215eee0ccf2e992c60b360de37c33a
Denis Laliberté <denis@tp1.ca>
2015-01-18 13:57:29 -0500

    sort two element

--- sort.rb
+++ sort.rb
@@ -1,3 +1,13 @@
 def sort(input)
-  input
+  if input.empty?
+    input
+  else 
+    a = input.slice(0,1)
+    b = input.slice(1,1)
+    if a.first > b.first
+      [b.first,a.first]
+    else
+      [a.first,b.first]
+    end
+  end
 end
--- sort_spec.rb
+++ sort_spec.rb
@@ -4,4 +4,7 @@ describe "sort" do
   it "return empty list" do
     expect(sort([])).to eq([])
   end
+  it "sort two elements" do
+    expect(sort([2,1])).to eq([1,2])
+  end
 end
