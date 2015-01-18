a47fc8d281511cd3aee3e7ff38b9231856eb3d62
Denis Laliberté <denis@tp1.ca>
2015-01-18 14:32:18 -0500

    sort 4 elements table

--- sort.rb
+++ sort.rb
@@ -1,14 +1,22 @@
 def sort(input)
   if (input.count < 2)
-    input
+    result =input
   else 
     middle = input.count / 2
     a = input.slice(0,middle)
     b = input.slice(middle,input.count)
-    if a.first > b.first
-      [b.first,a.first]
-    else
-      [a.first,b.first]
+    result = []
+    while not a.empty? or not b.empty?
+      if b.empty?
+        result.push(a.shift)
+      elsif a.empty?
+        result.push(b.shift)
+      elsif a.first > b.first
+        result.push(b.shift)
+      else 
+        result.push(a.shift)
+      end
     end
   end
+  result
 end
--- sort_spec.rb
+++ sort_spec.rb
@@ -10,7 +10,7 @@ describe "sort" do
   it "sort two elements" do
     expect(sort([2,1])).to eq([1,2])
   end
-  #it "sort four elements" do
-  #  expect(sort([4,3,2,1])).to eq([1,2,3,4])
-  #end
+  it "sort four elements" do
+    expect(sort([1,3,2,4])).to eq([1,2,3,4])
+  end
 end
